d5b828c171dc39f12eb7bafd236264f4233f9714
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-09-02 15:46:46 -0300

    feat(api): add bcrypt password hashing in UserServices
    - add bcrypt password hashing in UserSerives create and update methods
    for better security in storaging passwords in the database

--- toodoo-api/src/users/users.service.ts
+++ toodoo-api/src/users/users.service.ts
@@ -10,6 +10,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -20,6 +21,10 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto) {
     const user = this.usersRepository.create(createUserDto);
+
+    const salt = await bcrypt.genSalt();
+
+    user.password = await bcrypt.hash(user.password, salt);
 
     if (
       await this.usersRepository.exist({
@@ -50,6 +55,10 @@ export class UsersService {
 
   async update(id: string, updateUserDto: UpdateUserDto) {
     await this.idExists(id);
+
+    const salt = await bcrypt.genSalt();
+
+    updateUserDto.password = await bcrypt.hash(updateUserDto.password, salt);
 
     await this.usersRepository.update(id, updateUserDto);
   }
