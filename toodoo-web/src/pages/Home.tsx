57d0d12e8dd063b4b4814e9b4c6cc9a34e9b1834
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-09-05 11:29:09 -0300

    feat(web): add shadcn/ui configuration

--- toodoo-web/src/pages/Home.tsx
+++ toodoo-web/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Nav from "../components/Nav";
 
 const Home = () => {
@@ -11,24 +12,25 @@ const Home = () => {
         <header className="text-4xl font-bold text-violet-600 mb-4">
           Welcome to Toodoo
         </header>
-        <p className="text-lg text-gray-700 mb-8">
+        <p className="text-lg text-gray-500 mb-8">
           Keep your tasks private and securely manage your to-do list.
         </p>
         <div className="flex space-x-4">
-          <a
-            href="/register"
-            className="px-6 py-2 bg-violet-500 text-white 
-        rounded-full hover:bg-violet-600"
+          <Button
+            variant="default"
+            className="px-6 py-2 bg-violet-600 text-white 
+              rounded-full hover:bg-violet-700"
           >
-            Get Started
-          </a>
-          <a
-            href="/login"
-            className="px-6 py-2 bg-gray-500 text-white 
-        rounded-full hover:bg-gray-600"
+            <a href="/register">Get Started</a>
+          </Button>
+
+          <Button
+            variant="default"
+            className="bg-gray-500 text-white hover:bg-gray-600 rounded-full px-6 py-2"
+            asChild
           >
-            Login
-          </a>
+            <a href="/login">Login</a>
+          </Button>
         </div>
       </div>
     </>
