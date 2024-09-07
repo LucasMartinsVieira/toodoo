86d0e8e102d797bc91f2674ad48b09da382630d3
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-09-06 22:39:28 -0300

    chore(web): started adding support to dueDate

--- toodoo-web/src/pages/Tasks.tsx
+++ toodoo-web/src/pages/Tasks.tsx
@@ -2,33 +2,56 @@ import MainContainer from "@/components/Container";
 import Nav from "@/components/Nav";
 import { Button } from "@/components/ui/button";
 import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { getUserTasks } from "@/services/tasksService";
+import { cn } from "@/lib/utils";
+import { createTask, getUserTasks } from "@/services/tasksService";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { addDays, format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
 
-interface Task {
+export interface Task {
   id: string;
   title: string;
   description: string;
+  dueDate: Date | undefined;
   status: "completed" | "in-progress" | "pending";
 }
 
 const Tasks = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
   const [filter, setFilter] = useState("all");
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+  const [newTaskDueDate, setNewTaskDueDate] = useState<Date | undefined>(
+    new Date(),
+  ); // New state for due date
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const userTasks = JSON.parse(await getUserTasks());
+        const userTasks = await getUserTasks();
         setTasks(userTasks);
         setFilteredTasks(userTasks); // Set the filtered tasks to all to initialize
       } catch (error) {
@@ -56,8 +79,33 @@ const Tasks = () => {
     navigate(`/tasks/${taskId}`);
   };
 
-  const handleCreateTask = () => {
-    navigate(`/`);
+  const handleCreateTask = async () => {
+    const newTask = {
+      title: newTaskTitle,
+      description: newTaskDescription,
+      dueDate: newTaskDueDate,
+      status: "pending",
+    };
+
+    try {
+      await createTask(
+        newTask.title,
+        newTask.description,
+        newTask.status,
+        // newTask.dueDate,
+      );
+
+      setNewTaskTitle("");
+      setNewTaskDescription("");
+      // setNewTaskDueDate(new Date());
+
+      // TODO: Instead of reloading the window make the setTasks setter work as intended.
+      window.location.reload();
+      //setTasks((prevTasks) => [...prevTasks, createdTask]);
+    } catch (error) {
+      console.error("Error bicho " + error);
+      throw new Error("Failed to create task: " + error);
+    }
   };
 
   return (
@@ -106,14 +154,104 @@ const Tasks = () => {
               </SelectItem>
             </SelectContent>
           </Select>
-          <Button
-            onClick={handleCreateTask}
-            className="ml-4 px-4 py-2 rounded-full bg-gray-500 text-white hover:bg-gray-600"
-          >
-            Create Task
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                onClick={handleCreateTask}
+                className="ml-4 px-4 py-2 rounded-full bg-gray-500 text-white hover:bg-gray-600"
+              >
+                Create Task
+              </Button>
+            </DialogTrigger>
+            <DialogContent
+              aria-describedby="dialog-description"
+              className="bg-gray-800 text-white border border-gray-700 rounded-lg p-6"
+            >
+              <DialogHeader>
+                <DialogTitle className="text-xl font-bold text-violet-600">
+                  Create New Task
+                </DialogTitle>
+              </DialogHeader>
+              <div className="flex flex-col space-y-4">
+                <Input
+                  value={newTaskTitle}
+                  onChange={(e) => setNewTaskTitle(e.target.value)}
+                  type="text"
+                  placeholder="Task Title"
+                  className="px-4 py-2 rounded-full bg-gray-800 text-white 
+                    border border-gray-700 focus:outline-none 
+                    focus:ring-2 focus:ring-violet-600"
+                />
+                <Input
+                  value={newTaskDescription}
+                  onChange={(e) => setNewTaskDescription(e.target.value)}
+                  type="text"
+                  placeholder="Task Description"
+                  className="px-4 py-2 rounded-full bg-gray-800 text-white 
+                    border border-gray-700 focus:outline-none 
+                    focus:ring-2 focus:ring-violet-600"
+                />
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="default"
+                      className={cn(
+                        "w-full justify-start text-left font-normal bg-gray-600 hover:bg-gray-700",
+                        !newTaskDueDate && "text-muted-foreground",
+                      )}
+                    >
+                      <CalendarIcon className="mr-2 h-4 w-4" />
+                      {newTaskDueDate ? (
+                        format(newTaskDueDate, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    align="start"
+                    className="flex w-auto flex-col space-y-2 p-2 bg-gray-200 rounded-lg text-gray-800 "
+                  >
+                    <Select
+                      onValueChange={(value) =>
+                        setNewTaskDueDate(addDays(new Date(), parseInt(value)))
+                      }
+                    >
+                      <SelectTrigger className="bg-gray-300 text-gray-800">
+                        <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                      <SelectContent
+                        position="popper"
+                        className="bg-gray-300 text-gray-800"
+                      >
+                        <SelectItem value="0">Today</SelectItem>
+                        <SelectItem value="1">Tomorrow</SelectItem>
+                        <SelectItem value="3">In 3 days</SelectItem>
+                        <SelectItem value="7">In a week</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <div className="rounded-md border">
+                      <Calendar
+                        mode="single"
+                        selected={newTaskDueDate}
+                        onSelect={(date) => setNewTaskDueDate(date)}
+                        className="bg-gray-200 text-gray-800"
+                      />
+                    </div>
+                  </PopoverContent>
+                </Popover>
+
+                <Button
+                  onClick={handleCreateTask}
+                  className="bg-violet-600 text-white rounded-full py-2 hover:bg-violet-700"
+                >
+                  Create
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
-        <div className="px-6 sm:px-8 lg:px-10">
+        <div className="px-6 sm:px-8 lg:px-10 pb-8">
           <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {filteredTasks.map((task: Task) => (
               <li
--- toodoo-web/src/services/tasksService.ts
+++ toodoo-web/src/services/tasksService.ts
@@ -1,10 +1,12 @@
+import { Task } from "@/pages/Tasks";
+
 const API_URL = import.meta.env.VITE_API_URL;
-const GET_TASKS = `${API_URL}/tasks`;
+const TASKS_URL = `${API_URL}/tasks`;
+
+const token = localStorage.getItem("access_token");
 
 export const getUserTasks = async () => {
-  const token = localStorage.getItem("access_token");
-
-  const response = await fetch("http://localhost:3000/api/tasks", {
+  const response = await fetch(TASKS_URL, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -14,8 +16,33 @@ export const getUserTasks = async () => {
   });
 
   if (!response.ok) {
-    throw new Error("Failed to get tasks");
+    throw new Error("Failed to get tasks!");
   }
 
-  return JSON.stringify(await response.json());
+  return await response.json();
+};
+
+// TODO: Add dueDate to this function.
+export const createTask = async (
+  title: string,
+  description: string,
+  status: string,
+  // dueDate: Date | undefined,
+): Promise<Task> => {
+  const response = await fetch(TASKS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // prettier-ignore
+      "Authorization": `Bearer ${token}`,
+    },
+
+    body: JSON.stringify({ title, description, status }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create task!");
+  }
+
+  return await response.json();
 };
